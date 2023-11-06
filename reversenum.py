59760dc05b40d43615531b561db39e686437e5e0
programmingod <adit.sreenivasan@gmail.com>
2023-11-07 01:08:49 +0530

    Gives the reverse of a given number

---
+++ reversenum.py
@@ -0,0 +1,5 @@
+# The earlier versions of this code took way too many lines. 
+# I later figured out that it's just a 2-line problem
+
+n = input("Enter a number here: ")
+print ("The reverse of that number is", n[::-1])
