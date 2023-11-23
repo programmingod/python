a05e57a6cf023f274b27ca982e8f78edc07d42c4
programmingod <adit.sreenivasan@gmail.com>
2023-11-24 01:02:53 +0530

    A program that swaps the values of 2 variables

---
+++ Swap2Var.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first variable: "))
+b = int(input("Enter second variable: "))
+temp = a
+a = b
+b = temp
+print("After swap,")
+print("Value of first var: ", a)
+print("Value of second var: ", b)
