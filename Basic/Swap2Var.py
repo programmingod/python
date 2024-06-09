d3cd256898b5e4ef85deade8c507a25e1dc1612e
programmingod <adit.sreenivasan@gmail.com>
2024-06-09 20:24:01 +0530

    Added swapping without temp variable

--- Basic/Swap2Var.py
+++ Basic/Swap2Var.py
@@ -6,3 +6,12 @@ b = temp
 print("After swap,")
 print("Value of first var: ", a)
 print("Value of second var: ", b)
+
+# Let's try another way to do this, without a temp variable
+
+a = int(input("Enter first variable: "))
+b = int(input("Enter second variable: "))
+a,b = b,a
+print("After swap,")
+print("Value of first var: ", a)
+print("Value of second var: ", b)
